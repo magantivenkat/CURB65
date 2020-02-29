ad33b12643eb6f0ec6dccfc74302f7c6640ce732
Maganti <VenkataM@nedbank.co.za>
2020-02-29 22:35:52 +0200

    first commit from local branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
+import { MatSlideToggle, MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'CURB65Calc';
+  count = 0;
+  
+  chkConf = new FormControl();
+  onToggleChange(ob: MatSlideToggleChange) {
+    if (ob.checked) {
+      this.count += 1;
+    }
+    else {
+      this.count -= 1;
+    }
+    console.log(this.count);  
+
+  }  
+
 }
